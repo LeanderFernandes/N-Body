34b6b2c44971cc8238d1f349c1f64ac77e69326a
Leander Fernandes <leanderf05@gmail.com>
2022-11-25 12:57:36 +0000

    mpi testing

---
+++ practice/test_file.py
@@ -0,0 +1,41 @@
+from mpi4py import MPI
+import numpy as np
+import sys
+
+
+def double(x):
+    return x*2
+
+def main(nodes):
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    
+    #initialise arrays
+    array = np.arange(20)
+    split_arrays = np.array_split(array, nodes)
+    
+    if rank == 0:
+        data = split_arrays
+    else:
+        data = None
+        
+    #This is the part to for loop(time stepping loop)
+    for i in range(3):
+        #Comm scatter and gather
+        data = comm.scatter(data, root=0)
+
+        for i,number in enumerate(data):
+            data[i] = double(number)
+            
+        data = comm.gather(data, root=0)
+        if rank == 0:
+            print()
+            print(data)
+
+        
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(int(sys.argv[1]))
+    else:
+        print("Usage: Python {} <NODES> ".format(sys.argv[0]))
+    
